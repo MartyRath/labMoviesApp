81d099c21cc858fae8ab4b4edcd71d237a74e566
Marty Rath <martydavidger@gmail.com>
2024-08-18 11:01:11 +0100

    Updated fantasyMovieDetails to accept BaseFantasyMovieProps type

--- src/components/fantasyMovieDetails/index.tsx
+++ src/components/fantasyMovieDetails/index.tsx
@@ -1,8 +1,6 @@
-// FantasyMovieDetails.tsx
 import React from "react";
-import { useParams } from "react-router-dom";
-import { useFantasyMovies } from "../../contexts/fantasyMoviesContext";
 import { Paper, Chip, Typography, Box } from "@mui/material";
+import { BaseFantasyMovieProps } from "../../types/interfaces";
 
 const styles = {
   chipSet: {
@@ -28,53 +26,49 @@ const styles = {
   },
 };
 
-const FantasyMovieDetails: React.FC = () => {
-  const { id } = useParams<{ id: string }>();
-  const { fantasyMovies } = useFantasyMovies();
-
-  const movie = fantasyMovies.find((m) => m.id === Number(id));
-
-  if (!movie) {
-    return <Typography variant="h1">Fantasy Movie Not Found</Typography>;
-  }
-
+const FantasyMovieDetails: React.FC<BaseFantasyMovieProps> = ({
+  title,
+  overview,
+  genres,
+  release_date,
+  runtime,
+  production_companies,
+}) => {
   return (
     <Box sx={{ padding: 2 }}>
       <Typography variant="h4" component="h1" sx={styles.header}>
-        {movie.title}
+        {title}
       </Typography>
 
       <Typography variant="h6" component="h2" sx={styles.details}>
         Overview
       </Typography>
       <Typography variant="body1" sx={styles.details}>
-        {movie.overview}
+        {overview}
       </Typography>
 
-      {/* Display genres */}
       <Paper component="ul" sx={styles.chipSet}>
         <li>
           <Chip label="Genres" sx={styles.chipLabel} color="primary" />
         </li>
-        {movie.genres.map((genre, index) => (
-          <li key={index}>
+        {genres.map((genre) => (
+          <li key={genre}>
             <Chip label={genre} sx={styles.chip} />
           </li>
         ))}
       </Paper>
 
-      {/* Display additional details */}
       <Paper component="ul" sx={styles.chipSet}>
-        <Chip label={`Released: ${movie.release_date}`} sx={styles.chip} />
-        <Chip label={`Runtime: ${movie.runtime} min`} sx={styles.chip} />
-        {movie.production_companies.length > 0 && (
+        <Chip label={`Released: ${release_date}`} sx={styles.chip} />
+        <Chip label={`Runtime: ${runtime} min`} sx={styles.chip} />
+        {production_companies.length > 0 && (
           <>
             <Chip
               label="Production Companies"
               sx={styles.chipLabel}
               color="primary"
             />
-            {movie.production_companies.map((company, index) => (
+            {production_companies.map((company, index) => (
               <Chip key={index} label={company} sx={styles.chip} />
             ))}
           </>
