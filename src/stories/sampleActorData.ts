17b63dc6042838c427cf44165673d504c068fb42
Marty Rath <martydavidger@gmail.com>
2024-08-18 13:09:38 +0100

    Added workaround script for storybook cache issue

--- src/stories/sampleActorData.ts
+++ src/stories/sampleActorData.ts
@@ -4,9 +4,9 @@ export const actor = {
     biography: "Thomas Jeffrey Hanks (born July 9, 1956) is an American actor and filmmaker. Known for both his comedic and dramatic roles, Hanks is one of the most popular and recognizable film stars worldwide, and is widely regarded as an American cultural icon.",
     birthday: "1956-07-09",
     place_of_birth: "Concord, California, USA",
-    profile_path: "/2v9FVVBUrrkW2m3QOcYkuhq9A6o.jpg",
+    profile_path: "https://placehold.co/300x450",
     known_for_department: "Acting",
     imdb_id: "nm0000158",
-    poster_path: "/2v9FVVBUrrkW2m3QOcYkuhq9A6o.jpg",
+    poster_path: "https://placehold.co/300x450",
   };
   
